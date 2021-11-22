d939968ea6f27ba0317774113e7a1802930b7331
Alejandro Martín de León <alu0101015941@ull.edu.es>
2021-11-22 17:25:07 +0000

    Actualizadno cambios

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <fileRead msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import fileRead from './components/fileRead.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    fileRead
   }
 }
 </script>
